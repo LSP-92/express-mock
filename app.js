81d25573ff44c3768febf5313ea86e6d9f90df86
Luis Sanchez <luissanchez_1992@hotmail.com>
2021-12-21 00:02:18 +0100

    Changes

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/add', indexRouter);
+app.use('/', indexRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/', function (req, res, next) {
+router.post('/add', function (req, res, next) {
   console.log(req.body)
+  console.log(req.headers)
+  
+  
+  res.status(200).json({
+    code: 401,
+    message: "TEST",
+    error: ""
+  });
+});
+
+router.post('/auth', function (req, res, next) {
+  console.log(req.body)
+  console.log(req.headers)
+  res.setHeader("Authorization", "Bearer 123456789")
+  
   
   res.status(200).json({
     code: 200,
